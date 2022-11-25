b8a76b4c5036b56100aa77535de5ceb5e04f3db6
Andrew Crawford <crawfordcrawfordcrawford@gmail.com>
2022-11-25 19:02:30 +0000

    Trying out react

---
+++ reactproject1/src/index.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
+
+import { HubConnection } from '@microsoft/signalr';
+
+
+const hubConnection = new HubConnection('http://localhost:5134/chatHub');
+
+connection.invoke("SendMessageToGroup", "hello", "").catch(function (err) {
+    return console.error(err.toString());
+});
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
